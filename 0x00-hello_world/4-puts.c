0cc88ac058b1ec7da6c3bd5ac16321d9a22fd930
Adetoyosi <omoletoyosi1997@gmail.com>
2023-03-16 17:34:43 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
-/*
- * main - print a line of code using puts
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
  *
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like a multilingul puzzle");
 	return (0);
 }
