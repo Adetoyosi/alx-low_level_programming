07fc8f8f5a8b8fde9316e49bfb0eec87876e4105
Adetoyosi <omoletoyosi1997@gmail.com>
2023-03-28 05:18:58 -0700

    Lack of computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * _puts - prints a string, followed by a new line to stdout
- * @str: string to print
+ * _puts - prints strings
+ * @str: function parameter
  * Return: 0
  */
 
@@ -10,7 +9,7 @@ void _puts(char *str)
 {
 	int i;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-		_putchar(str[i]);
-	_putchar('\n');
+	for (i = 0 ; str[i] !=  '\0' ; i++)
+		putchar(str[i]);
+	putchar('\n');
 }
