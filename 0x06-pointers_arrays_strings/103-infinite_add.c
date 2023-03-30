a555eff7bec97554488bcea31d7dff74970e55fa
Adetoyosi <omoletoyosi1997@gmail.com>
2023-03-30 14:35:06 -0700

    Noise is a Buffer

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i;
+	int j;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+
