0b477768086979b798af964a90ab12bf61067cb7
Adetoyosi <omoletoyosi1997@gmail.com>
2023-05-02 08:29:14 -0700

    Free Safe Version

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * -r - reallocates memory for an array of pointers
+ * _ra - reallocates memory for an array of pointers
  *  to the nodes in a linked list
  *  @list: the old list to append
  *  @size: size of the new list (always more than the old list)
@@ -11,9 +11,9 @@
  *
  *  Return: pointer to the new list
  */
-const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-	const listint_t **newlist;
+	listint_t **newlist;
 	size_t i;
 
 	newlist = malloc(size * sizeof(listint_t *));
@@ -31,30 +31,34 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 
 /**
  * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * @head: pointer to the first node in the linked list
  *
  * Return: number of elements in the freed list
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
 	size_t i, num = 0;
-	const listint_t **list = NULL;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	while (head != NULL)
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
 	{
 		for (i = 0; i < num; i++)
 		{
-			if (head == list[i])
+			if (*head == list[i])
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
+				*head = NULL;
 				free(list);
 				return (num);
 			}
 		}
 		num++;
-		list = _r(list, num, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
 	free(list);
 	return (num);
